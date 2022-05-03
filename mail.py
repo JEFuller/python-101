9470cd2c33e9ecda43b4672fcfd89a0f9746ab13
Dave Tapley <dave@tapley.com>
2022-05-03 07:53:05 -0700

    HTML mail

--- mail.py
+++ mail.py
@@ -11,18 +11,22 @@ def send(sender, receiver, subject, body, image=None):
     message["To"] = receiver
     message["Subject"] = subject
 
-    text = MIMEText(body, "plain")
-
-    message.attach(text)
-
     if image:
         image.seek(0)
         file = MIMEBase('application', "octet-stream")
         file.set_payload(image.read())
         encode_base64(file)
-        file.add_header('Content-Disposition',
-                        'attachment; filename="rain.png"')
+        file.add_header('Content-Disposition', 'attachment; filename="rain.png"')
+        file.add_header('Content-ID', '<rain>')
         message.attach(file)
+
+        html = MIMEText(f'<div>{body}</div><img src="cid:rain">', 'html')
+        message.attach(html)
+
+    else:
+        text = MIMEText(body, "plain")
+        message.attach(text)
+
 
     server = smtplib.SMTP("smtp.mailtrap.io", 2525)
     server.login("2ff5f8d9b897ae", "86e4d0018894a2")
